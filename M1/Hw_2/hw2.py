1eb377831d37b66b82e9a6db6381b09cdea3b297
Ale <alexandra.mita@student.upt.ro>
2026-05-13 17:33:11 +0300

    doina hw2

---
+++ M1/Hw_2/hw2.py
@@ -0,0 +1,23 @@
+#Exercise 1
+print("Astazi ma duc la \"facultate\"")
+print("/*\\/*\\*/*\\/*\\")
+print("Python")
+print("\\./\\./\\./\\./")
+print("P\ty\tt\th\to\tn")
+
+#Exercise 2
+name= input("Enter your name: ")
+age= input("Enter your age: ")
+
+print("Cum te numesti?"+ name)
+print("Ce varsta ai?" + age)
+print("Ceau! Deci te-ai nascut in " + str(2025 - int(age)))
+
+#Exercise 3
+string = input("Introduceti un sir: ")
+print("Lungimea sirului este {length}".format(length=len(string)))
+print(f"Lungimea sirului este {len(string)}")
+print("Lungimea sirului este " + str(len(string)))
+print("Lungimea sirului este", len(string))
+
+#Exercise 4
